213865464c0f1dc6989a47f8c4553bdce8104762
adolf <vadimschool7@mail.ru>
2020-11-18 17:52:47 +0300

    added O(n) difficult

--- scripts/script.js
+++ scripts/script.js
@@ -10,43 +10,33 @@ function showFile(input) {
 }
 function findLengthBW() {
     let arraySubstring=getAllSubstring(' ' + document.getElementsByTagName('input')[1].value + ' ', ' ' + document.getElementsByTagName('input')[2].value + ' ');
-    console.log(arraySubstring)
-    if(!arraySubstring.substrings1.length||!arraySubstring.substrings2.length) {
-        document.getElementById('answer').innerHTML='Нет таких слов';
-        return;
-    }
-    findMinLength(arraySubstring.substrings1,arraySubstring.substrings2);
+    document.getElementById('answer').innerHTML='Минимальное расстояние: ' + arraySubstring;
 }
 
 function getAllSubstring(str1, str2) {
-    let substrings1=[];
-    let substrings2=[];
+    let min=stringFile.length;
+    let lastIndex1;
+    let lastIndex2;
     let j,k=0;
     for(let i=0;i<stringFile.length;i++) {
         if(stringFile[i]===str1[j]) {
             j++;
-            if(j===str1.length) 
-                substrings1.push(i-str1.length+1);
+            if(j===str1.length) {
+                if(i-str1.length-lastIndex2<min) 
+                    min=i-str1.length-lastIndex2
+            lastIndex1=i-2;
+            }
         }
         else j=0;
         if(stringFile[i]===str2[k]) {
             k++;
-            if(k===str2.length) 
-                substrings2.push(i-str2.length+1);
+            if(k===str2.length) {
+                if(i-str2.length-lastIndex1<min) 
+                    min=i-str2.length-lastIndex1
+            lastIndex2=i-2;
+            }
         }
         else k=0;
     }
-    console.log(substrings1, substrings2)
-    return {substrings1, substrings2}
-}
-
-function findMinLength(arr1,arr2) {
-    let min=stringFile.length;
-    for(let i=0;i<arr1.length;i++) {
-        for(let j=0;j<arr2.length;j++) {
-            if(Math.abs(arr1[i]-arr2[j])-arr1.length<min) 
-                min=Math.abs(arr1[i]-arr2[j])-document.getElementsByTagName('input')[1].value.length;
-        }
-    }
-    document.getElementById('answer').innerHTML='Минимальное расстояние: ' + min;
+    return min;
 }
