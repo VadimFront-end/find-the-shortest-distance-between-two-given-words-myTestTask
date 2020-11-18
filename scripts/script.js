495dc6a8de9bb52b32f55d59bdc7528c5ca799c2
adolf <vadimschool7@mail.ru>
2020-11-18 18:08:11 +0300

    added error processing

--- scripts/script.js
+++ scripts/script.js
@@ -9,34 +9,36 @@ function showFile(input) {
     };
 }
 function findLengthBW() {
-    let arraySubstring=getAllSubstring(' ' + document.getElementsByTagName('input')[1].value + ' ', ' ' + document.getElementsByTagName('input')[2].value + ' ');
-    document.getElementById('answer').innerHTML='Минимальное расстояние: ' + arraySubstring;
+    let arraySubstring=getMinDistance(' ' + document.getElementsByTagName('input')[1].value + ' ', ' ' + document.getElementsByTagName('input')[2].value + ' ');
+    if(arraySubstring!==-1)document.getElementById('answer').innerHTML='Минимальное расстояние: ' + arraySubstring;
+    else document.getElementById('answer').innerHTML='Нет таких слов';
 }
 
-function getAllSubstring(str1, str2) {
+function getMinDistance(str1, str2) {
     let min=stringFile.length;
-    let lastIndex1;
-    let lastIndex2;
+    let lastIndexFirstWord;
+    let lastIndexSecondWord;
     let j,k=0;
     for(let i=0;i<stringFile.length;i++) {
         if(stringFile[i]===str1[j]) {
             j++;
             if(j===str1.length) {
-                if(i-str1.length-lastIndex2<min) 
-                    min=i-str1.length-lastIndex2
-            lastIndex1=i-2;
+                if(i-str1.length-lastIndexSecondWord<min) 
+                    min=i-str1.length-lastIndexSecondWord
+            lastIndexFirstWord=i-2;
             }
         }
         else j=0;
         if(stringFile[i]===str2[k]) {
             k++;
             if(k===str2.length) {
-                if(i-str2.length-lastIndex1<min) 
-                    min=i-str2.length-lastIndex1
-            lastIndex2=i-2;
+                if(i-str2.length-lastIndexFirstWord<min) 
+                    min=i-str2.length-lastIndexFirstWord
+            lastIndexSecondWord=i-2;
             }
         }
         else k=0;
     }
-    return min;
+    if(lastIndexFirstWord===undefined||lastIndexSecondWord===undefined) return -1;
+    else return min;
 }
